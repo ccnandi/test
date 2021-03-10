ffdd6f9149737b901c3a2175cb8ca34cbeaf4b8d
ccnandi <80395708+ccnandi@users.noreply.github.com>
2021-03-10 17:48:26 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,8 +2,22 @@
 
 using namespace std;
 
+void CFeature::CFeature() 
+  : m_data(0)
+  {
+  }
+
+void CFeature::fnDisplay() 
+{
+  cout << __FUNCTION__ << m_data;
+}
+
+
 int main () {
   cout << __FILE__ << " : " << __LINE__ << " : " << __FUNCTION__ << endl;
+  
+  CFeature cobj(77);
+  cobj.fnDisplay();
  
   return 0;
 }
