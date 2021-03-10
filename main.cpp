320b450af83f71c07ed62062fe68d0d90ec7e02c
ccnandi <80395708+ccnandi@users.noreply.github.com>
2021-03-10 17:22:08 +0530

    beautify

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main () {
-  cout << __FILE__ << " : " << __LINE__<< " : " << __FUNCTION__ << endl;
+  cout << __FILE__ << " : " << __LINE__ << " : " << __FUNCTION__ << endl;
  
   return 0;
 }
